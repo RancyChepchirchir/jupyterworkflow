dc6705689f97e5bb1405aefc722cb3fb28d85de7
muxuezi <muxuezi@gmail.com>
2017-03-18 16:19:20 +0800

    change url

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from urllib.request import urlretrieve
 
 import pandas as pd
@@ -8,29 +8,29 @@ FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=
 
 def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
     """Download and cache the fremont data
-    
+
     Parameters
     ----------
     filename : string (optional)
-	location to save the data
+        location to save the data
     url: string (optional)
-	web location of the data
+        web location of the data
     force_download : bool (optional)
-	if true, force redownload of data
-    
+        if true, force redownload of data
+
     Returns
     -------
     data : pandas.DataFrame
-	The fremont bridge data
+        The fremont bridge data
     """
     if not os.path.exists(filename):
         urlretrieve(FREMONT_URL, 'Fremont.csv')
     data = pd.read_csv('Fremont.csv', index_col='Date')
 
     try:
-    	data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
     except TypeError:
-    	data.index = pd.to_datetime(data.index)
+        data.index = pd.to_datetime(data.index)
     data.columns = ['west', 'east']
     data['Total'] = data.sum(axis=1)
     return data
--- jupyterworkflow/test/test_data.py
+++ jupyterworkflow/test/test_data.py
@@ -2,6 +2,7 @@ from jupyterworkflow.data import get_fremont_data
 import pandas as pd
 import numpy as np
 
+
 def test_fremont_data():
     data = get_fremont_data()
     assert all(data.columns == ['west', 'east', 'Total'])
